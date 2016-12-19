756aad4cda54001552d6818adb4e1d3941c4c931
Chris Humphreys <christian.humphreys@gmail.com>
2016-12-19 11:54:55 +0000

    logic

---
+++ src/com/example/intro/logic/ForMain.java
@@ -0,0 +1,12 @@
+package com.example.intro.logic;
+
+public class ForMain {
+    
+    public static void main(String[] args) {
+        
+        for(int counter=0; counter<10; counter++){
+            System.out.println(counter);
+        }
+        
+    }
+}
---
+++ src/com/example/intro/logic/IfMain.java
@@ -0,0 +1,16 @@
+package com.example.intro.logic;
+
+public class IfMain {
+    
+    public static void main(String[] args) {
+        
+        boolean coupleContract = true;
+        
+        if (coupleContract) {
+            System.out.println("This is a couple contract");
+        } else {
+            System.out.println("This is a single contract");
+        }
+        
+    }
+}
